d48ca6ebe1c5431d9a08599578a2bc07f1ad9bfa
Kevin Liu <you@example.com>
2023-06-29 21:53:41 -0400

    Add links to the navbar that can be used to navigate to the all-campuses view and the all-students view

--- src/App/App.js
+++ src/App/App.js
@@ -4,10 +4,29 @@ import "./App.css";
 import Home from "../pages/Home";
 import Students from "../pages/students";
 import Campuses from "../pages/campuses";
+import { LinkContainer } from 'react-router-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <LinkContainer to="/"><Navbar.Brand>Home</Navbar.Brand></LinkContainer>
+        <Nav className="me-auto">
+          <LinkContainer to="/campuses">
+            <Nav.Link>Campuses</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/students">
+            <Nav.Link>Students</Nav.Link>
+          </LinkContainer>
+        </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+
+        </Navbar.Collapse>
+      </Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/students" element={<Students />} />
