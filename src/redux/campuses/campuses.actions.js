4d2d2fe9576f17ee4352b58d1753a2b6e0ac39f4
Kevin Liu <you@example.com>
2023-07-06 15:46:36 -0400

    Testing Deployment

--- src/redux/campuses/campuses.actions.js
+++ src/redux/campuses/campuses.actions.js
@@ -56,7 +56,7 @@ export const fetchSingleCampusThunk = (campusId) =>{
   return async (dispatch)=>{
       try {
           console.log("FETCHSINGLECAMPUSTHUNK IS FIRING");
-          const response = await axios.get(`${process.env.REACT_APP_API_URL}api/campuses/${campusId}`);
+          const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/campuses/${campusId}`);
           console.log("FETCHSINGLECAMPUSTHUNK COMPLETED");
           dispatch(fetchSingleCampus(response.data));
       } catch (error) {
@@ -68,7 +68,7 @@ export const fetchSingleCampusThunk = (campusId) =>{
 export const createCampusThunk = (campus) => {
   return async (dispatch) => {
       try {
-          const response = await axios.post(`${process.env.REACT_APP_API_URL}api/campuses/add`, campus);
+          const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/campuses/add`, campus);
           dispatch(createCampus(response.data));
       } catch (error) {
           console.log(error); 
@@ -79,7 +79,7 @@ export const createCampusThunk = (campus) => {
 export const editCampusThunk = (campus) => {
   return async (dispatch) => {
     try {
-      const response = await axios.put(`${process.env.REACT_APP_API_URL}api/campuses/edit/${campus.id}`, campus);
+      const response = await axios.put(`${process.env.REACT_APP_API_URL}/api/campuses/edit/${campus.id}`, campus);
       dispatch(editCampus(response.data));
     } catch (error) {
       console.error(error);
@@ -90,7 +90,7 @@ export const editCampusThunk = (campus) => {
 export const deleteCampusThunk = (campusId) => {
   return async (dispatch) => {
     try {
-      await axios.delete(`${process.env.REACT_APP_API_URL}api/campuses/delete/${campusId}`);
+      await axios.delete(`${process.env.REACT_APP_API_URL}/api/campuses/delete/${campusId}`);
       dispatch(deleteCampus(campusId));
     } catch (error) {
       console.error(error);
